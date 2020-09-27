69c5dfa4bd78ab7f047b93730be9ff6e604f975d
Tanzim Mokammel <tanzim.mokammel@moonfare.com>
2020-09-27 16:29:40 -0400

    basic bst

--- spellbook.hs
+++ spellbook.hs
@@ -1,6 +1,5 @@
 speller_with_idx :: [[Char]] -> Int -> Int -> [Char]
-
-speller_with_idx [] idx len = []
+speller_with_idx [] _ _ = []
 speller_with_idx (w : ws) idx len
   | w : ws == w : [] = (if idx == 0 then "" else " and ") ++ [w !! 0] ++ " is for " ++ w
   | w : ws == w : ws = [w !! 0] ++ " is for " ++ w ++ (if idx == len -2 then "" else ", ") ++ speller_with_idx ws (idx + 1) len
